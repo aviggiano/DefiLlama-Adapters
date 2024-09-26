e103ad279a68f989b7a251565753eb830562cfd9
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2024-09-26 10:46:09 -0300

    Update variable names

--- projects/size-credit/index.js
+++ projects/size-credit/index.js
@@ -5,8 +5,8 @@ const config = {
   base: {
     WETH_USDC: {
       Size: '0xC2a429681CAd7C1ce36442fbf7A4a68B11eFF940',
-      CollateralToken: ADDRESSES.base.WETH,
-      BorrowToken: ADDRESSES.base.USDbC,
+      UnderlyingCollateralToken: ADDRESSES.base.WETH,
+      UnderlyingBorrowToken: ADDRESSES.base.USDbC,
       BorrowAToken: '0x4e65fe4dba92790696d040ac24aa414708f5c0ab',
       DebtToken: '0xb0a00c4b3d77c896f46dc6b204695e22de7a185d'
     },
@@ -21,7 +21,7 @@ async function getBorrowed(api, size, borrowToken) {
 
 Object.keys(config).forEach(chain => {
   module.exports[chain] = {
-    tvl: sumTokensExport({ tokens: [config[chain].CollateralToken, config[chain].BorrowAToken], owner: config[chain].Size }),
-    borrowed: (api) => getBorrowed(api, config[chain].BorrowToken)
+    tvl: sumTokensExport({ tokens: [config[chain].UnderlyingCollateralToken, config[chain].UnderlyingBorrowAToken], owner: config[chain].Size }),
+    borrowed: (api) => getBorrowed(api, config[chain].BorrowAToken)
   }
 })
